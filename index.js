5360288177253ced82db516d1b730808d4e9c50b
Himanshu_Kumar <krhimanshu0208@gmail.com>
2024-07-26 19:22:02 +0530

    initial commit

---
+++ index.js
@@ -0,0 +1,29 @@
+import express from "express";
+import bodyParser from "body-parser";
+import axios from "axios";
+
+const app = express();
+const port = 3000;
+
+app.use(express.static("public"));
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.get("/", (req, res) => {
+  res.render("index.ejs");
+});
+
+app.get("/exprience", (req, res) => {
+  res.render("experience.ejs");
+});
+
+app.get("/contact", (req, res) => {
+  res.render("contact.ejs");
+});
+
+app.get("/about", (req, res) => {
+  res.render("about.ejs");
+});
+
+app.listen(port, () => {
+  console.log(`The server is running on port ${port}`);
+});
